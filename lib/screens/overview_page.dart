f29ba8b2b48097feec0edc3eae810ebbee11e468
EftiarHKhan <mdeftiar.official@gmail.com>
2022-10-20 00:13:45 +0600

    home and overview page

--- lib/screens/overview_page.dart
+++ lib/screens/overview_page.dart
@@ -12,10 +12,49 @@ class OverviewPage extends StatefulWidget {
 class _OverviewPageState extends State<OverviewPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Overview page'),
-        centerTitle: true,
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/home.png'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 30, top: 80),
+              child: const Text(
+                'Semesters',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontFamily: 'lato',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 30, top: 150),
+              child: Row(
+                children: [
+                  Column(
+                    children: const [
+                      Text(
+                        '2022',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'lato',
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
