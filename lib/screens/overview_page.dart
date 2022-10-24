345baafa76ff8adbbf7723b51a8a6c837f06bff0
EftiarHKhan <mdeftiar.official@gmail.com>
2022-10-24 14:37:01 +0600

    listview

--- lib/screens/overview_page.dart
+++ lib/screens/overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iubat_cgpa/screens/home_page.dart';
 
 class OverviewPage extends StatefulWidget {
   static String routeName = 'OverviewPage';
@@ -23,30 +24,36 @@ class _OverviewPageState extends State<OverviewPage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Stack(
+        body: Column(
           children: [
             Container(
-              padding: EdgeInsets.only(left: 30, top: 80),
-              child: const Text(
-                'Semesters',
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontFamily: 'lato',
-                  fontWeight: FontWeight.bold,
-                ),
+              padding: EdgeInsets.only(left: 30, top: 60),
+              child: Row(
+                children: const [
+                  Text(
+                    'Semesters',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontFamily: 'lato',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
-
-            ListView.separated(
-              padding: const EdgeInsets.only(left: 30, top: 150),
-              itemCount: 3,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  child: Column(
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+              child: ListView.separated(
+                padding: const EdgeInsets.only(left: 30, top: 5),
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
                     children: [
                       Container(
-                        child: Column(
+                        child: Row(
                           children: [
                             Text(
                               '${year[index]}',
@@ -59,60 +66,85 @@ class _OverviewPageState extends State<OverviewPage> {
                           ],
                         ),
                       ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.only(left: 40, right: 60),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: const Color.fromRGBO(38, 38, 38, 2),
-                        ),
-                        child: Row(
-                          children: [
-                            Text(
-                              '${cgpa[index]}',
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
+
+                      ListView.separated(
+                        shrinkWrap: true,
+                        primary: false,
+                        padding: const EdgeInsets.only(left: 0, top: 20),
+                        itemCount: 3,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.only(left: 10, right: 90),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color.fromRGBO(38, 38, 38, 2),
                             ),
-                            Spacer(),
-                            Text(
-                              '${semester[index]}',
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                              ),
+                            child: Row(
+                              children: [
+                                Text(
+                                  '${cgpa[index]}',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  '${semester[index]}',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 30,
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) =>
+                            const Divider(),
                       ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+
+                      // Container(
+                      //   padding: EdgeInsets.all(10),
+                      //   margin: EdgeInsets.only(left: 40, right: 60),
+                      //   decoration: BoxDecoration(
+                      //     borderRadius: BorderRadius.circular(10),
+                      //     color: const Color.fromRGBO(38, 38, 38, 2),
+                      //   ),
+                      //   child: Row(
+                      //     children: [
+                      //       Text(
+                      //         '${cgpa[index]}',
+                      //         style: const TextStyle(
+                      //           color: Colors.white,
+                      //           fontSize: 20,
+                      //         ),
+                      //       ),
+                      //       Spacer(),
+                      //       Text(
+                      //         '${semester[index]}',
+                      //         style: const TextStyle(
+                      //           color: Colors.white,
+                      //           fontSize: 20,
+                      //         ),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ),
                     ],
-                  ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+              ),
             ),
-            // Container(
-            //   padding: EdgeInsets.only(left: 30, top: 150),
-            //   child: Row(
-            //     children: [
-            //       Column(
-            //         children: const [
-            //           Text(
-            //             '2022',
-            //             style: TextStyle(
-            //               color: Colors.white,
-            //               fontFamily: 'lato',
-            //               fontSize: 25,
-            //               fontWeight: FontWeight.bold,
-            //             ),
-            //           ),
-            //         ],
-            //       ),
-            //     ],
-            //   ),
-            // ),
           ],
         ),
       ),
